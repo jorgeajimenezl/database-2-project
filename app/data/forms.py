56900423b13b7d4994772ee89382c0d33b9ee616
Jorge Alejandro Jimenez Luna <jorgeajimenezl17@gmail.com>
2022-06-12 17:31:25 -0400

    Added register truck logic

--- app/data/forms.py
+++ app/data/forms.py
@@ -1,12 +1,12 @@
 from flask_wtf import FlaskForm
-from wtforms import ValidationError, StringField, FloatField, SelectField
-from wtforms.validators import DataRequired, EqualTo
+from wtforms import ValidationError, StringField, DecimalField, SelectField
+from wtforms.validators import DataRequired, EqualTo, NumberRange
 
 class TruckForm(FlaskForm):
     model = StringField("Model", validators=[DataRequired()])
-    weight = FloatField("Weight", validators=[DataRequired()])
+    weight = DecimalField("Weight", validators=[DataRequired()])
     fuel_type = SelectField("Fuel Type", choices=["Gasoline", "Diesel"], validators=[DataRequired()])
-
+    driver = SelectField("Driver", validators=[DataRequired()])
     
     
 
--- app/data/routes.py
+++ app/data/routes.py
@@ -1,11 +1,33 @@
-from flask import render_template
+from crypt import methods
+from lib2to3.pgen2 import driver
+from flask import flash, render_template
 
 from .forms import TruckForm
+from .models import Driver, Employee, Truck
 from ..auth import login_required
 from . import data_bp
 
-
-@data_bp.route("/register_truck")
+@data_bp.route("/register_truck", methods=["GET", "POST"])
 def register_truck():
     form = TruckForm()
+    drivers = Driver.query.all()
+    form.driver.choices = {x.employee.name: x.employee_id for x in drivers}
+
+    if form.validate_on_submit():
+        employee = Employee.get_by_id(form.driver.data)
+        if employee:
+            Truck.create(
+                weight=float(form.weight.data),
+                model=form.model.data,
+                fuel_type=form.model.data,
+                driver_id=form.driver.data,
+            )
+            flash("Truck registration successful!!")
+        else:
+            flash("Invalid driver", "danger")
+
     return render_template("data/register_truck.html", form=form)
+
+@data_bp.route("/register_employee", methods=["GET", "POST"])
+def register_employee():
+    pass
